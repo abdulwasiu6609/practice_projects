276c2293a5577ccf34ab2d5314d1f56ebe9804b1
abdulwasiu6609 <abdulwasiu6609@gmail.com>
2026-03-18 12:53:14 +0100

    updated chip

--- app/uicomponents/chips.jsx
+++ app/uicomponents/chips.jsx
@@ -1,16 +1,15 @@
 import { FaPaintBrush } from "react-icons/fa";
 
 export default function BasicChip() {
-
-
-    return (
-        <div className="size-48  shadow bg-indigo-300 mx-auto relative ">
-            {/* chip */}
-            <div className="bg-indigo-800 w-fit px-4 py-1 rounded-full absolute top-1 right-1 text-sm text-white flex items-center gap-2">Indigo-300 
-
-                <div className="bg-indigo-900 p-2 rounded-full"><FaPaintBrush size={8} /></div>
-            </div>
-
+  return (
+    <div className="size-48  shadow bg-indigo-300 mx-auto relative ">
+      {/* chip */}
+      <div className="bg-indigo-800 w-fit px-4 py-1 rounded-full absolute top-1 right-1 text-sm text-white flex items-center gap-2">
+        Indigo-300
+        <div className="bg-indigo-900 p-2 rounded-full">
+          <FaPaintBrush size={8} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
